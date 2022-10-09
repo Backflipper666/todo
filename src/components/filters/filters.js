ace10d1892b7068f067520fb91f1b0b4bb0f3695
Backflipper666 <elderly619@gmail.com>
2022-10-09 19:41:19 +0600

    decompose filters and place it in components

---
+++ src/components/filters/filters.js
@@ -0,0 +1,17 @@
+const Filters = () => {
+    return (
+        <ul className="filters">
+        <li>
+          <button className="selected">All</button>
+        </li>
+        <li>
+          <button>Active</button>
+        </li>
+        <li>
+          <button>Completed</button>
+        </li>
+      </ul>
+    )
+}
+
+export default Filters
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,18 +1,10 @@
- const Footer =()=>{
+import Filters from "../filters/filters"
+
+const Footer =()=>{
     return (
         <footer className="footer">
           <span className="todo-count">1 items left</span>
-          <ul className="filters">
-            <li>
-              <button className="selected">All</button>
-            </li>
-            <li>
-              <button>Active</button>
-            </li>
-            <li>
-              <button>Completed</button>
-            </li>
-          </ul>
+          <Filters/>
           <button className="clear-completed">Clear completed</button>
         </footer>
     )
