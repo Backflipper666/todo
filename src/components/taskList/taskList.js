6f565c946758f0519297fdd802918d97ac886f97
Backflipper666 <elderly619@gmail.com>
2022-10-09 20:13:14 +0600

    decompose task

--- src/components/taskList/taskList.js
+++ src/components/taskList/taskList.js
@@ -1,17 +1,9 @@
+import Task from "../task/task"
+
 const TaskList = () => {
     return (
         <ul className="todo-list">
-              <li className="completed">
-                <div className="view">
-                  <input className="toggle" type="checkbox"/>
-                  <label>
-                    <span className="description">Completed task</span>
-                    <span className="created">created 17 seconds ago</span>
-                  </label>
-                  <button className="icon icon-edit"></button>
-                  <button className="icon icon-destroy"></button>
-                </div>
-              </li>
+            <Task/>
               <li className="editing">
                 <div className="view">
                   <input className="toggle" type="checkbox"/>
