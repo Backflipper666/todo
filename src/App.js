15fe886db0af68b6306ae64ade8e8f69be13e7f4
Backflipper666 <elderly619@gmail.com>
2022-10-09 19:11:34 +0600

    add closing slash to input tags

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Footer from './components/footer';
 
 function App() {
   return (
-    <section className="todoapp">
+ 
+
+<section className="todoapp">
       <header className="header">
         <h1>todos</h1>
-        <input className="new-todo" placeholder="What needs to be done?" autofocus>
+        <input className="new-todo" placeholder="What needs to be done?" autofocus/>
       </header>
       <section className="main">
         <ul className="todo-list">
           <li className="completed">
             <div clasNames="view">
-              <input className="toggle" type="checkbox">
+              <input className="toggle" type="checkbox"/>
               <label>
                 <span className="description">Completed task</span>
                 <span className="created">created 17 seconds ago</span>
@@ -24,7 +26,7 @@ function App() {
           </li>
           <li className="editing">
             <div className="view">
-              <input className="toggle" type="checkbox">
+              <input className="toggle" type="checkbox"/>
               <label>
                 <span className="description">Editing task</span>
                 <span className="created">created 5 minutes ago</span>
@@ -32,11 +34,11 @@ function App() {
               <button className="icon icon-edit"></button>
               <button className="icon icon-destroy"></button>
             </div>
-            <input type="text" className="edit" value="Editing task">
+            <input type="text" className="edit" value="Editing task"/>
           </li>
           <li>
             <div className="view">
-              <input className="toggle" type="checkbox">
+              <input className="toggle" type="checkbox"/>
               <label>
                 <span className="description">Active task</span>
                 <span className="created">created 5 minutes ago</span>
@@ -63,7 +65,7 @@ function App() {
         </footer>
       </section>
     </section>
-    
+ 
   );
 }
 
