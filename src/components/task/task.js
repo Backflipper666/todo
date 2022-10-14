0bf1a8e405c0c3b7f5346b126cfee9ab5a1d33f9
Backflipper666 <elderly619@gmail.com>
2022-10-15 04:57:26 +0600

    remove compl and edit from task.js

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -8,55 +8,28 @@ export default class Task extends React.Component {
     grandParent.classList.toggle('completed');
   };
 
+  edit = (evt) => {
+    //parentDiv is only used to get GrandParent element
+    let parentDiv = evt.target.parentElement;
+    let grandParent = parentDiv.parentElement;
+    grandParent.classList.toggle('editing');
+    // grandParent.classList.add('editing');
+  };
+
   render() {
-    const { edit, regular, compl } = this.props;
-    if (edit) {
-      return (
-        <li className="editing">
-          <div className="view">
-            <input className="toggle" type="checkbox" />
-            <label>
-              <span className="description">Editing task</span>
-              <span className="created">created 5 minutes ago</span>
-            </label>
-            <button className="icon icon-edit"></button>
-            <button className="icon icon-destroy"></button>
-          </div>
-          <input
-            type="text"
-            className="edit"
-            /* value="Editing task"  */ defaultValue="Editing task"
-          />
-        </li>
-      );
-    } else if (regular) {
-      return (
-        <li>
-          <div className="view">
-            <input className="toggle" type="checkbox" onClick={this.toggle} />
-            <label>
-              <span className="description">Active task</span>
-              <span className="created">created 5 minutes ago</span>
-            </label>
-            <button className="icon icon-edit"></button>
-            <button className="icon icon-destroy"></button>
-          </div>
-        </li>
-      );
-    } else if (compl) {
-      return (
-        <li className="completed">
-          <div className="view">
-            <input className="toggle" type="checkbox" onClick={this.toggle} />
-            <label>
-              <span className="description">Completed task</span>
-              <span className="created">created 17 seconds ago</span>
-            </label>
-            <button className="icon icon-edit"></button>
-            <button className="icon icon-destroy"></button>
-          </div>
-        </li>
-      );
-    }
+    const { edit, regular, compl, label } = this.props;
+    return (
+      <li>
+        <div className="view">
+          <input className="toggle" type="checkbox" onClick={this.toggle} />
+          <label>
+            <span className="description">{label}</span>
+            <span className="created">created 5 minutes ago</span>
+          </label>
+          <button className="icon icon-edit" onClick={this.edit}></button>
+          <button className="icon icon-destroy"></button>
+        </div>
+      </li>
+    );
   }
 }
--- src/components/taskList/taskList.js
+++ src/components/taskList/taskList.js
@@ -3,9 +3,9 @@ import Task from '../task/task';
 const TaskList = () => {
   return (
     <ul className="todo-list">
-      <Task compl />
-      <Task edit />
-      <Task regular />
+      <Task regular label="Active task" />
+      <Task edit label="Editing task" />
+      <Task regular label="Active task" />
     </ul>
   );
 };
